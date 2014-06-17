d21245d98fae7168264fe0407468eb58648ed87d
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2014-06-17 04:33:54 -0300

    Alterações na área de contato

--- chapeado/index.php
+++ chapeado/index.php
@@ -46,10 +46,31 @@
 		<img src="image/exemplo.png">
 
 		<div class="Message">
-			<h2 id="">simplesmente <br> magnifico!</h2>
-			<img src="image/exemplo.png">
+
+			<h3 id="">Sobre a casa:</h3>
+			<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+
+
+			<div id="Products">
+				<img class="imageProduct" src="image/Desert.jpg"/>
+				<img class="imageProduct" src="image/Desert.jpg"/>
+			</div>
+
 		</div>
 
+		<div id="Contact">
+
+			<h4 id="">Contato:</h4>
+
+			<div id="ContactInfo">
+
+
+					<img src="image/facebook.png"/>
+					<img src="image/2.png"/>
+
+			</div>
+
+		</div>
 </div>
 
 </header>
