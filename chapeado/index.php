6f52c68366dfe2a16094bb1007e2dffc1dd66e02
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2014-06-17 02:55:02 -0300

    Primeiro Commit

---
+++ chapeado/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+
+<title>Churrascaria Senador - Chapeado</title>
+
+<meta name="description" content="Churrascaria Senador - Chapeado">
+
+<meta name="author" content="Studio Noctus">
+
+<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+
+<link rel="stylesheet" href="css/style.css">
+
+<!--[if IE]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+</head>
+
+<body>
+
+<div id="Header">
+
+	<div id="Wrap">
+
+		<img id="Logo" height="100px" src="image/logo.png"/>
+
+		<div id='cssmenu'>
+			<ul>
+			   <li class='active'><a href='index.html'><span>Home</span></a></li>
+			   <li><a href='#'><span>Sobre a casa</span></a></li>
+			   <li><a href='#'><span>Contato</span></a></li>
+			</ul>
+		</div>
+	</div>
+
+</div>
+
+<div id="Content">
+
+		<h2 id="">simplesmente <br> magnifico!</h2>
+
+		<img src="image/exemplo.png">
+
+		<div class="Message">
+			<h2 id="">simplesmente <br> magnifico!</h2>
+			<img src="image/exemplo.png">
+		</div>
+
+</div>
+
+</header>
+
+</body>
+
+</html>
